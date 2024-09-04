b850b71614f0b9ccc70f70e098779cbee459163a
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-04 17:52:47 +0200

    add file for task 2 to project 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,11 @@
 
 from sys import argv
 
-if len(argv) == 1:
-    print("{} arguments.".format(len(argv)-1))
-else:
-    print("{} arguments:".format(len(argv)-1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
-
 if __name__ == "__main__":
-    pass
+
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv)-1))
+    else:
+        print("{} arguments:".format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
