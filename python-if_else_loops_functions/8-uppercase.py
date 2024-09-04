11055373816397fd9fe004b2e4c93c388b12d19c
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-04 10:35:34 +0200

    add files for task 8 project 2

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
