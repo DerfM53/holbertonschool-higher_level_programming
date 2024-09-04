96cc72e0234de988ef0a947bffb2447717432175
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-04 09:32:09 +0200

    add file for task 5 project 2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i <= 9:
-        print(f"0{i}", end=", ")
+        print("0{}".format(i), end=", ")
     else:
-        print(f"{i}", end=", " if i < 99 else "\n")
+        print("{}".format(i), end=", " if i < 99 else "\n")
