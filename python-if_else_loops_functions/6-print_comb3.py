82f8a1f77d7e18ca0ed8a645df088589778370ef
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-04 09:38:29 +0200

    correction file task 6 project 2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num1 in range(9):
     for num2 in range(num1 + 1, 10):
-        print(f"{num1}{num2}", end=", " if num1 != 8 or num2 != 9 else "")
+        end_char = ", " if num1 != 8 or num2 != 9 else ""
+        print("{}{}".format(num1, num2), end=end_char)
 print()
