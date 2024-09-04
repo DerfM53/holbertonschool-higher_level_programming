9bfaebf70e4054aec681573aaf1ea9bfe730cffa
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-04 16:27:14 +0200

    add files for task 1 to project 3

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,14 +4,10 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
-sum = sub(a, b)
-print("{} + {} = {}".format(a, b, sum))
-sum = mul(a, b)
-print("{} + {} = {}".format(a, b, sum))
-sum = div(a, b)
-print("{} + {} = {}".format(a, b, sum))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sub(a, b)))
+print("{} + {} = {}".format(a, b, mul(a, b)))
+print("{} + {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     pass
