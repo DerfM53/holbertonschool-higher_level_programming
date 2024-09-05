885b31688fcaef08a19e740290600bbf35c0f18e
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-05 10:44:52 +0200

    add files for task 3 project 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,12 @@
 
 from sys import argv
 
-if len(argv) == 1:
-    print("{}".format(len(argv)-1))
-else:
-    result = 0
-    for i in range(1, len(argv)):
-        result += int(argv[i])
-    print("{}".format(result))
+if __name__ == "__main__":
+
+    if len(argv) == 1:
+        print("{}".format(len(argv)-1))
+    else:
+        result = 0
+        for i in range(1, len(argv)):
+            result += int(argv[i])
+        print("{}".format(result))
