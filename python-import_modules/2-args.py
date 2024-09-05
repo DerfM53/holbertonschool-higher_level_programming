4b19eb8371518efa806b9a86f08cbac55dc822f5
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-05 13:13:28 +0200

    correction file task 2 project 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
-    if len(argv) == 1:
-        print("{} arguments.".format(len(argv)-1))
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv)-1))
     else:
-        print("{} arguments:".format(len(argv)-1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
