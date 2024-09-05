01a5d56d1677b16cfa7473747168c3122e48fbce
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-05 11:21:47 +0200

    add file for task 4 project 3

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import importlib.util
 
-# Charger le fichier compilé .pyc
-spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
-hidden_4 = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(hidden_4)
+if __name__ == "__main__":
 
-# Lister les noms définis dans le module
-names = dir(hidden_4)
+    spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
 
-# Filtrer et afficher les noms qui ne commencent pas par __
-for name in sorted(names):
-    if not name.startswith("__"):
-        print(name)
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
