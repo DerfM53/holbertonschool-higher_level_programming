3f8d27ae53fa39c89681fd8707927613b96620ec
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-06 11:23:30 +0200

    add file for task advanced 6 project 3

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 
-import calculator_1
 import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+
+    a = sys.argv[1]
+    operator = sys.argv[2]
+    b = sys.argv[3]
+
+    if operator not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(a)
+    b = int(b)
+
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-        elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-        elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-        elif sys.argv[2] == "/":
-            print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        result = div(a, b)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
