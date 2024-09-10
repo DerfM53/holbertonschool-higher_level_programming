2d6ad2ed6632a00910db5a431893a70c1c0b32a3
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-10 15:07:13 +0200

    correction file task 0 forget map

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = [element ** 2 for element in row]
-        new_matrix.append(new_row)
+    def square_row(row):
+        return list(map(lambda element: element ** 2, row))
+    squared_rows = map(square_row, matrix)
+    new_matrix = list(squared_rows)
     return new_matrix
