ede7b6ae56d44fa9050fe0e87d1d673c0589d200
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-10 15:39:31 +0200

    add file for task 12 project 1

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    total = 0
+    prev_value = 0
+    if not isinstance(roman_string, str or roman_string is None):
+        return 0
+    else:
+        for char in reversed(roman_string):
+            value = roman_numerals[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        return total
