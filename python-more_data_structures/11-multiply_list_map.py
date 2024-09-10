b2473a206143eebafd1ee7ddc6d62e7461f35f54
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-10 15:45:01 +0200

    correction file for task 11 project 1

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda element: element * number, my_list))
-    return new_list
+    return list(map(lambda element: element * number, my_list))
