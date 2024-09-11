456a17264b70718abc0df103378d0ce9cc875bc9
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-11 08:46:57 +0200

    add file task 1 project 2

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 def safe_print_integer(value):
     try:
         if isinstance(value, int):
-            print("{}".format(value))
+            print("{:d}".format(value))
             return True
     except ValueError:
         return False
