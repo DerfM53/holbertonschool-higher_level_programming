9dbae6afebd3528901ec3bb5a9e99ca7bcb14b71
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-11 12:31:20 +0200

    correction file task 9 project 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
+    if len(my_list) == 0:
         return None
-    value = 0
+    value = my_list[0]
     for i in my_list:
         if i > value:
             value = i
-        else:
-            continue
     return value
