3066704f5d6e1715c2464c5c259e05dc247305b2
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-13 11:04:32 +0200

    add file task 1 project 3

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,22 +21,20 @@ def matrix_divided(matrix, div):
                    or if div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
-    if not isinstance(matrix, list) or not all(
-        isinstance(row, list) for row in matrix
-    ):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-    if len(set(len(row) for row in matrix)) != 1:
-        raise TypeError("Each row of the matrix must have the same size")
-    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    def matrix_row(row):
-        return [round(element / div, 2) for element in row]
-    return list(map(matrix_row, matrix))
+    def matrix_divided(matrix, div):
+        if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+        if not all(len(row) == len(matrix[0]) for row in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
+    
+        if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+    
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+    
+    return [[round(elem / div, 2) for elem in row] for row in matrix]
