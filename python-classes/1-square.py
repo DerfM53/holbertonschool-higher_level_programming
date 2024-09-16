3ea8ef31903572da99926ea777448b5b6845c14f
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-16 18:21:50 +0200

    correction files for task1 project 0

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
 class Square:
-    """
-    Class of squares
-    """
+    """A class that defines a square."""
+
     def __init__(self, size):
+        """Initialize with an integer for value."""
         self.__size = size
