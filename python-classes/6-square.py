edc2af361fce2500db70ee0a79bc0968112d629b
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-16 18:00:10 +0200

    add files for task 6 project 0

---
+++ python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class of squares
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.__position = position
+
+    def area(self):
+        """
+        Return area of square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        print a square with symbol #
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        [print() for _ in range(self.__position[1])]
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    @property
+    def size(self):
+        value = self.__size
+        return value
+
+    @property
+    def position(self):
+        value = self.__position
+        return value
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(n, int) and n >= 0 for n in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
