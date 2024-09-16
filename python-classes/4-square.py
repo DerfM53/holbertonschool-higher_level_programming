68bc0f58ee5abc3593a1d02bf3597f4bf447d7b4
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-16 16:44:29 +0200

    add files task 4 project 0

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class of squares
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
+    @property
+    def size(self):
+        value = self.__size
+        return value
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+
