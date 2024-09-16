b51e127ed8bdf2f7dcc955db0fd1f51f026268b5
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-16 18:18:47 +0200

    correction files for task 0 project 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
 class Square:
-    """
-    A class that defines a square.
+    """A class that defines a square."""
 
-    This class is a basic implementation of a square. Currently, it doesn't
-    have any attributes or methods beyond the constructor.
-    """
     def __init__(self):
-        """
-        Initializes a new instance of the Square class.
-
-        The constructor is empty (using 'pass') as it currently doesn't
-        set any attributes or perform any operations.
-        """
+        """Initialize a new Square instance."""
         pass
