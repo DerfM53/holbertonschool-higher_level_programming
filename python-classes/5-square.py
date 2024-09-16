d64ee5d010efc0b06ce58020bbbb37c7022d5f40
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-16 17:10:09 +0200

    add files for task 5 project 0

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class of squares
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Return area of square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        print a square with symbol #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__size):
+                for b in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        value = self.__size
+        return value
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
