fbb0c98d344a6360df9f1eaffa72d96959aedc72
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-17 10:43:48 +0200

    file for task 0 to project 1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that define a rectangle class"""
+
+
+class Rectangle:
+    """A class that define a rectangle"""
+
+    def __init__(self):
+        """Initialize a new Square instance"""
+        pass
