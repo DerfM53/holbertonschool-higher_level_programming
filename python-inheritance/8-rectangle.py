c17030cd1893e76271687fe050ff5011435ec7bb
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-23 17:55:28 +0200

    correction file for task 8 project 0

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -29,9 +29,12 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """A base class of BaseGeometry for Rectangle classes"""
+    """
+    A class representing a rectangle,
+    inheriting from BaseGeometry.
+    """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
