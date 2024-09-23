8bcb4a568eb85b0594f0120b13ad72956ebe5a1c
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-23 17:28:26 +0200

    correction file for task 2 project 0

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,9 +4,5 @@ def is_same_class(obj, a_class):
     """
     Return True if the type of obj is
     idently with a_class
-    else return False
     """
-    if type(obj) is a_class:
-        return True
-    else:
-        return False
+    return type(obj) is a_class
