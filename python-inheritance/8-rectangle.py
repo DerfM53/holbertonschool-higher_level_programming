54351af9cb7011297ccf3f94e6c0adf390c8ef9b
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-23 16:33:31 +0200

    add files for task 8 project 0

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A base class for geometry-related classes."""
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: Always raises an exception with the message
+                       "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate that a value is a positive integer.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A base class of BaseGeometry for Rectangle classes"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
