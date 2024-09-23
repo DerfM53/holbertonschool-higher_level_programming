8e28d74c9c84dc8a447e80bd7ad0870ef39a5b48
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-23 15:14:08 +0200

    add files for task 6 project 0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module create a class"""
+
+
+class BaseGeometry:
+    """This class create intance"""
+    def area(self):
+        """area return a message for raise exception"""
+        raise Exception("area() is not implemented")
