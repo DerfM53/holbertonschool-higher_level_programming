b7f7d090b524487155df13abbdd3f8604dbc22a8
DerfM53 <frederic.mieuzet@gmail.com>
2024-09-25 08:53:34 +0200

    correction file for task 8 project 0

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle(BaseGeometry):
     inheriting from BaseGeometry.
     """
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
