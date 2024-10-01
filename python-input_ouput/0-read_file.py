55238f88ac48bd7c2049fe63db2d598a223361f2
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-01 11:03:58 +0200

    correction file for task 0 project 0

--- python-input_ouput/0-read_file.py
+++ python-input_ouput/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
     This function open and read filename in inpout
     an print the contain of file in ouput
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r" ,encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
