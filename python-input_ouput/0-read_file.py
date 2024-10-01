5397c5f04a40dbfd8565e6f65a2683c4f00d8047
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-01 11:06:41 +0200

    correction file for task 0 project 0

--- python-input_ouput/0-read_file.py
+++ python-input_ouput/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end="")
+    print(read_data, end="")
