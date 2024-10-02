d5fdc611225bced1cb5c3a8e8c1c358de29577bf
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-02 08:32:53 +0200

    add file for task 0 project 0

--- python-input_ouput/0-read_file.py
+++ python-input_ouput/0-read_file.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""This module containt a function"""
+"""
+Module containing a function to read
+and print the contents of a file.
+"""
 
 
 def read_file(filename=""):
     """
-    This function open and read filename in inpout
-    an print the contain of file in ouput
+    Reads a text file (UTF8) and prints its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+
+    The function uses the 'with' statement to ensure proper file handling.
+    It does not manage file permissions or handle exceptions for non-existent files.
     """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data, end="")
+        print(read_data, end="")
