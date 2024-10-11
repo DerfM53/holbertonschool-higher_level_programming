1b4bdca08caeeef7d4230a6dff353f16264a61f0
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-11 15:40:22 +0200

    correction file task 4 project 0

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,26 +4,32 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {}
+users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
 
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
 @app.route('/data')
 def json_response():
-    return jsonify(users)
+    user = list(users.keys())
+    return jsonify(user)
 @app.route('/status')
 def status():
     return "ok"
 @app.route('/users/<username>')
 def user_data(username):
-    return jsonify(users[username])
+    if username not in users:
+        return {"error": "User not found"}
+    else:
+        return jsonify(users[username])
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    users = request.get_json()
-    print(users)
-    return jsonify(users)
-
+    new_user = request.get_json()
+    if 'username' not in new_user:
+        return jsonify({"error": "Username is required"}), 400
+    username = new_user['username']
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__": app.run()
