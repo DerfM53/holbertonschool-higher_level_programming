ccdd1a016559feb464781494de632fdef8e22c56
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-28 12:42:58 +0100

    correction file task 2 and 2 project 0

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- script to delete database if it exist
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
--- script that lists all the tables of a database in your MySQL server
-SHOW TABLES FROM mysql;
+-- script that lists all the tables of a database.
+SHOW TABLES;
