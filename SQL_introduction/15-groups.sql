6404a193cfc277b52704049a6951c3b8e9263102
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-28 12:53:04 +0100

    correction file task 15 and 16 project 0

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
+-- Script that lists the number of records with the same score in the table second_table
+-- Displays the score and the number of records for each score
+-- Results are sorted by the number of records in descending order
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
+-- Script that lists all records of the table second_table
+-- Excludes rows without a name value
+-- Displays the score and name, ordered by descending score
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
