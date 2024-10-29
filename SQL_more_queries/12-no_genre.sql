98623b1bea3ec76aacebc7786e253b50db83c49b
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-29 17:39:57 +0100

    complete file for task 12 project 1

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- List values of tv_shows only don't have genres_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
