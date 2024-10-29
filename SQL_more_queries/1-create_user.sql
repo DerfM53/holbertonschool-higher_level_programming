47c02330ea157d65b03a4ee9d9fd35f3eac8989a
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-29 09:14:11 +0100

    correction file for task1 project 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,8 @@
 ALTER USER IF EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Create user user_0d_1 if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Revoke all existing privileges to ensure only SELECT will be granted
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
 -- Grant all privileges to user_0d_1 on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 -- Apply the changes
