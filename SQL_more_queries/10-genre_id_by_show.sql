38c46fc26e6d8157bd4a0295dfda96ecd71e70e6
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-29 17:16:10 +0100

    complete file for task 10 project 1

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- List values of tv_shows with genres id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
