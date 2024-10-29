3211d69d25e17670adc0fcfa03f8b0edbce67291
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-29 15:08:14 +0100

    complete file for task 7 project 0

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use database
+USE hbtn_0d_usa;
+-- Create table and add columns with foreign key of primary key of states tables
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+    );
