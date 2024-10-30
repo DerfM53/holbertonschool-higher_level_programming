c1418c72aee9c3c46276816a15f2ee614ee7700a
DerfM53 <frederic.mieuzet@gmail.com>
2024-10-30 09:09:07 +0100

    correction fiel for task 1 project 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,3 @@
--- Replace or create the password for the user if it exists
-ALTER USER IF EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Create user user_0d_1 if it doesn't already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Revoke all existing privileges to ensure only SELECT will be granted
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
--- Grant all privileges to user_0d_1 on all databases and tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- Apply the changes
-FLUSH PRIVILEGES;
+-- script that creates the MySQL server user user_0d_1 and grant all previligies to it
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* to "user_0d_1"@"localhost";
