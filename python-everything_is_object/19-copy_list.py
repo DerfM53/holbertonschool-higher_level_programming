f3532366635a2534783e664b317b1b3821cac779
DerfM53 <frederic.mieuzet@gmail.com>
2024-12-02 12:17:17 +0100

    new directory and files complete until task28 for project 0

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def copy_list(a_list):
+    newlist = a_list[:]
+    return newlist
---
+++ python-everything_is_object/essai.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def assign_valeur(n, v):
+    n = v
+    print(n)
+    print(v)
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+assign_valeur(l1, l2)
+print(l1)
+
